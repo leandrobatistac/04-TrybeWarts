06275ed8e21cc7581fb111498a72c4edf7711b84
Leandro Batista <leandrobcrv@gmail.com>
2022-09-14 22:18:13 -0300

    Requisitos 19 e 20

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // ===================================================================================
-// Conferir Login e Senha
+// Variaveis Globais
 
 const emailLogin = document.getElementById('inputLogin');
 const senhaLogin = document.getElementById('inputSenha');
 const botaoLogin = document.getElementById('botaoLogin');
+const botaoEnviar = document.getElementById('submit-btn');
+const checkEnvio = document.getElementById('agreement');
+const contador = document.getElementById('counter');
+const areaTexto = document.getElementById('textarea');
 let confereEmail = false;
 let confereSenha = false;
+
+// ===================================================================================
+// Função para conferir o email do Login
 
 function conferirLogin() {
   if (emailLogin.value === 'tryber@teste.com') {
@@ -13,11 +20,17 @@ function conferirLogin() {
   }
 }
 
+// ===================================================================================
+// Função para conferir a senha do Login
+
 function conferirSenha() {
   if (senhaLogin.value === '123456') {
     confereSenha = true;
   }
 }
+
+// ===================================================================================
+// Função para enviar informações e conferir Login/Senha
 
 function conferirLoginSenha() {
   conferirLogin();
@@ -30,3 +43,26 @@ function conferirLoginSenha() {
 }
 
 botaoLogin.addEventListener('click', conferirLoginSenha);
+
+// ===================================================================================
+// Ativar/Desativar Botão de Submit
+
+function checarBotao() {
+  if (checkEnvio.checked) {
+    botaoEnviar.disabled = false;
+  } else {
+    botaoEnviar.disabled = true;
+  }
+}
+
+checkEnvio.addEventListener('click', checarBotao);
+
+// ===================================================================================
+// Função para contar de Caracteres
+
+const caracteresMaximo = parseInt(contador.innerText, 10);
+function subtrairCaracteres() {
+  contador.innerText = caracteresMaximo - areaTexto.value.length;
+}
+
+areaTexto.addEventListener('keyup', subtrairCaracteres);
